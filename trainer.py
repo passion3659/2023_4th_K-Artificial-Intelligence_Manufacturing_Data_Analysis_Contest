1eec88901db4bf7739e0c1c00b917e2d30b4034a
Jeongyeol Yang <89252263+passion3659@users.noreply.github.com>
2024-11-05 03:19:32 +0900

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,62 +1,58 @@
 import os
-import torch
-import torch.nn as nn
 import numpy as np
+import pandas as pd
 from tqdm import tqdm
 
+import torch
+import torch.nn as nn
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, roc_auc_score
+
+
 class Trainer():
-    def __init__(self, model, optimizer, loss_fn=nn.MSELoss(), label_scaling = False, save_model_path='model_saved/best_model.pth'):
+    def __init__(self, model, optimizer, loss_fn=nn.CrossEntropyLoss(), save_model_path='model_saved_dl/best_model.pth'):
         super().__init__()
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.model = model.to(self.device)  # 모델을 GPU로 이동
+        self.model = model.to(self.device)  
         self.optimizer = optimizer
         self.loss_fn = loss_fn
-        self.label_scaling = label_scaling 
         self.save_model_path = save_model_path
 
-        # 모델을 저장할 디렉토리 확인 및 생성
         save_dir = os.path.dirname(self.save_model_path)
         if save_dir and not os.path.exists(save_dir):
             os.makedirs(save_dir)
 
-    def train_step(self, inputs, targets):
-        # 모델을 학습 모드로 설정
+    def train_step(self, inputs, targets, attention_mask):
         self.model.train()
 
-        # 데이터를 GPU로 이동
-        inputs, targets = inputs.to(self.device), targets.to(self.device)
+        inputs, targets, attention_mask = inputs.to(self.device), targets.to(self.device), attention_mask.to(self.device)
 
-        # 옵티마이저 초기화
         self.optimizer.zero_grad()
 
-        # 모델의 예측값 계산
-        outputs = self.model(inputs)
+        outputs = self.model(inputs, attention_mask=attention_mask) 
 
-        # 손실 계산
         loss = self.loss_fn(outputs, targets)
 
-        # 역전파 수행 및 가중치 갱신
         loss.backward()
         self.optimizer.step()
 
         return loss.item()
 
-    def eval_step(self, inputs, targets):
-        # 모델을 평가 모드로 설정
+    def eval_step(self, inputs, targets, attention_mask):
         self.model.eval()
 
-        # 데이터를 GPU로 이동
-        inputs, targets = inputs.to(self.device), targets.to(self.device)
+        inputs, targets, attention_mask = inputs.to(self.device), targets.to(self.device), attention_mask.to(self.device)
 
-        # 평가 시에는 gradients 계산 필요 없음
         with torch.no_grad():
-            # 모델의 예측값 계산
-            outputs = self.model(inputs)
+            outputs = self.model(inputs, attention_mask=attention_mask)  
 
-            # 손실 계산
             loss = self.loss_fn(outputs, targets)
 
-        return loss.item()
+            _, preds = torch.max(outputs, dim=1)
+            accuracy = (preds == targets).float().mean().item()
+
+        return loss.item(), accuracy
+
 
     def fit(self, train_loader, val_loader, epochs=10):
         best_val_loss = float('inf')
@@ -64,183 +60,90 @@ class Trainer():
         for epoch in range(epochs):
             train_loss = 0.0
             val_loss = 0.0
+            val_accuracy = 0.0
 
             train_loop = tqdm(train_loader, desc=f"Epoch {epoch+1}/{epochs} - Training", leave=False)
-            for inputs, targets, _, _, _ in train_loop:
-                loss = self.train_step(inputs, targets)
+            for inputs, targets, attention_mask in train_loop:  
+                loss = self.train_step(inputs, targets, attention_mask)  
                 train_loss += loss
                 train_loop.set_postfix(train_loss=train_loss / len(train_loader))
 
             val_loop = tqdm(val_loader, desc=f"Epoch {epoch+1}/{epochs} - Validation", leave=False)
-            for inputs, targets, _, _, _ in val_loop:
-                loss = self.eval_step(inputs, targets)
+            for inputs, targets, attention_mask in val_loop: 
+                loss, accuracy = self.eval_step(inputs, targets, attention_mask)  
                 val_loss += loss
-                val_loop.set_postfix(val_loss=val_loss / len(val_loader))
+                val_accuracy += accuracy
+                val_loop.set_postfix(val_loss=val_loss / len(val_loader), val_accuracy=val_accuracy / len(val_loader))
 
             avg_train_loss = train_loss / len(train_loader)
             avg_val_loss = val_loss / len(val_loader)
-            print(f"Epoch {epoch+1}, Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}")
+            avg_val_accuracy = val_accuracy / len(val_loader)
+            print(f"Epoch {epoch+1}, Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}, Val Accuracy: {avg_val_accuracy:.4f}")
 
-            # 최저 검증 손실 업데이트 및 모델 저장
             if avg_val_loss < best_val_loss:
                 best_val_loss = avg_val_loss
                 torch.save(self.model.state_dict(), self.save_model_path)
                 print(f"Best model saved with Val Loss: {best_val_loss:.4f} at {self.save_model_path}")
 
-    def inference(self, data_loader, scaler=None):
-        # Check if label_scaling is True but no scaler is provided
-        if self.label_scaling and scaler is None:
-            raise ValueError("Label scaling is enabled, but no scaler is provided for inverse transformation.")
+    def inference(self, data_loader):
+        self.model.eval()
 
-        self.model.eval()  # 모델을 평가 모드로 설정
-        pred_dict = {}
-        actual_dict = {}
-        
-        channel_1_outputs = []
-        channel_2_outputs = []
-        actual_channel_1_outputs = []
-        actual_channel_2_outputs = []
+        predictions = []
+        actual_values = []
 
-        with torch.no_grad():  # Inference 시 gradient 계산 비활성화
-            for i, (X, y, id, time_X, time_y) in enumerate(tqdm(data_loader)):
-                
-                inputs = X.to(self.device) # b,t,f
-                targets = y.to(self.device) # b,t,f
-                
-                # 모델 예측 수행
-                outputs = self.model(inputs) # b,t,f
+        with torch.no_grad():  
+            for inputs, targets, attention_mask in data_loader:  
+                inputs, targets, attention_mask = inputs.to(self.device), targets.to(self.device), attention_mask.to(self.device)
 
-                # 모델의 출력 채널 수에 따라 분리
-                if outputs.shape[2] == 2:  # 채널이 2개인 경우
-                    predicted_channel_1 = outputs[:, :, 0].view(-1, 1)  # 채널 1의 예측값
-                    predicted_channel_2 = outputs[:, :, 1].view(-1, 1)  # 채널 2의 예측값
+                outputs = self.model(inputs, attention_mask=attention_mask)  
 
-                    # 실제 값을 채널별로 분리
-                    actual_channel_1 = targets[:, :, 0].view(-1, 1)  # 채널 1의 실제값
-                    actual_channel_2 = targets[:, :, 1].view(-1, 1)  # 채널 2의 실제값
+                predicted_classes = torch.argmax(outputs, dim=1).cpu()
+                true_classes = targets.cpu()
 
-                    # 예측값과 실제값을 리스트에 추가
-                    channel_1_outputs.append(predicted_channel_1)
-                    channel_2_outputs.append(predicted_channel_2)
-                    actual_channel_1_outputs.append(actual_channel_1)
-                    actual_channel_2_outputs.append(actual_channel_2)
-                elif outputs.shape[2] == 1:  # 채널이 1개인 경우
-                    if self.label_scaling and scaler:
-                        targets = targets.cpu().numpy()
-                        batch_size, time_stamp, feature = targets.shape
-                        targets = targets.reshape(-1, feature)
-                        targets = scaler.inverse_transform(targets)
-                        targets = targets.reshape(batch_size, time_stamp, feature)
-                        targets = np.transpose(targets, (0, 2, 1))
-                        targets = torch.tensor(targets)
-                        
-                        outputs = outputs.cpu().numpy()
-                        batch_size, time_stamp, feature = outputs.shape
-                        outputs = outputs.reshape(-1, feature)
-                        outputs = scaler.inverse_transform(outputs)
-                        outputs = outputs.reshape(batch_size, time_stamp, feature)
-                        outputs = np.transpose(outputs, (0, 2, 1))
-                        outputs = torch.tensor(outputs)
-                    else:
-                        targets = targets.permute(0,2,1)
-                        outputs = outputs.permute(0,2,1)
-                    
-                    for batch_idx in range(outputs.size(0)):  
-                        file_id = id[batch_idx].item()  
-                        time_index = time_y[batch_idx] 
-                        
-                        if file_id not in pred_dict:
-                            pred_dict[file_id] = {}
-                        
-                        pred_dict[file_id][time_index] = outputs[batch_idx].squeeze(0).cpu().numpy()
-                        
-                    for batch_idx in range(targets.size(0)):  
-                        file_id = id[batch_idx].item() 
-                        time_index = time_y[batch_idx]
-                        
-                        if file_id not in actual_dict:
-                            actual_dict[file_id] = {}
-                        
-                        actual_dict[file_id][time_index] = targets[batch_idx].squeeze(0).cpu().numpy()
+                predictions.append(predicted_classes)
+                actual_values.append(true_classes)
 
-        return pred_dict, actual_dict
+        predictions = torch.cat(predictions, dim=0).numpy()
+        actual_values = torch.cat(actual_values, dim=0).numpy()
 
-    def evaluate_metrics(self, val_loader, scaler=None, model_path='model_saved/best_model.pth'):
-        # Check if label_scaling is True but no scaler is provided
-        if self.label_scaling and scaler is None:
-            raise ValueError("Label scaling is enabled, but no scaler is provided for inverse transformation.")
+        return predictions, actual_values
 
-        # 모델 가중치 로드
+    def evaluate_metrics(self, val_loader, model_path='model_saved_dl/best_model.pth'):
         self.model.load_state_dict(torch.load(model_path))
-        self.model.to(self.device)  # 모델을 GPU로 이동
-        self.model.eval()  # 평가 모드로 전환
+        self.model.to(self.device)  
+        self.model.eval()  
 
-        true_values_channel1 = []
-        true_values_channel2 = []
-        predictions_channel1 = []
-        predictions_channel2 = []
-        
-        with torch.no_grad():  # 평가 시에는 gradient 계산 불필요
-            for inputs, targets, _, _, _ in val_loader:
-                # 데이터를 GPU로 이동
-                inputs, targets = inputs.to(self.device), targets.to(self.device)
+        true_values = []
+        predictions = []
+        prediction_scores = []
 
-                # 모델 예측 수행
-                outputs = self.model(inputs)
+        with torch.no_grad():  
+            for inputs, targets, attention_mask in val_loader: 
+                inputs, targets, attention_mask = inputs.to(self.device), targets.to(self.device), attention_mask.to(self.device)
 
-                # 채널 수에 따라 분리
-                if outputs.shape[2] == 2:  # 채널이 2개인 경우
-                    predicted_channel1 = outputs[:, :, 0].cpu().numpy()
-                    predicted_channel2 = outputs[:, :, 1].cpu().numpy()
+                outputs = self.model(inputs, attention_mask=attention_mask) 
 
-                    true_channel1 = targets[:, :, 0].cpu().numpy()
-                    true_channel2 = targets[:, :, 1].cpu().numpy()
+                predicted_classes = torch.argmax(outputs, dim=1).cpu().numpy()
+                true_classes = targets.cpu().numpy()
+                
+                prediction_scores.extend(outputs[:, 1].cpu().numpy()) 
 
-                    true_values_channel1.append(true_channel1)
-                    true_values_channel2.append(true_channel2)
-                    predictions_channel1.append(predicted_channel1)
-                    predictions_channel2.append(predicted_channel2)
+                true_values.extend(true_classes)
+                predictions.extend(predicted_classes)
 
-                elif outputs.shape[2] == 1:  # 채널이 1개인 경우
-                    predicted_channel1 = outputs[:, :, 0].cpu().numpy()
-                    true_channel1 = targets[:, :, 0].cpu().numpy()
+        true_values = np.array(true_values)
+        predictions = np.array(predictions)
+        prediction_scores = np.array(prediction_scores)
 
-                    true_values_channel1.append(true_channel1)
-                    predictions_channel1.append(predicted_channel1)
+        accuracy = accuracy_score(true_values, predictions)
+        precision = precision_score(true_values, predictions, average='weighted')
+        recall = recall_score(true_values, predictions, average='weighted')
+        f1 = f1_score(true_values, predictions, average='weighted')
 
-        # 리스트를 배열로 변환
-        true_values_channel1 = np.concatenate(true_values_channel1, axis=0)
-        predictions_channel1 = np.concatenate(predictions_channel1, axis=0)
+        auroc = roc_auc_score(true_values, prediction_scores)
 
-        # 채널 2가 있는 경우에만 처리
-        if true_values_channel2 and predictions_channel2:
-            true_values_channel2 = np.concatenate(true_values_channel2, axis=0)
-            predictions_channel2 = np.concatenate(predictions_channel2, axis=0)
+        conf_matrix = confusion_matrix(true_values, predictions)
 
-        # Apply inverse_transform if label_scaling is True and scaler is provided
-        if self.label_scaling and scaler:
-            print('label_scaler applied for inverse transform')
-            predictions_channel1 = scaler.inverse_transform(predictions_channel1)
-            true_values_channel1 = scaler.inverse_transform(true_values_channel1)
-
-            if true_values_channel2 and predictions_channel2:
-                predictions_channel2 = scaler.inverse_transform(predictions_channel2)
-                true_values_channel2 = scaler.inverse_transform(true_values_channel2)
-
-        # 각 채널에 대해 MSE, RMSE, MAE, MAPE 계산
-        def calculate_metrics(predictions, true_values):
-            mse = np.mean((predictions - true_values) ** 2)
-            rmse = np.sqrt(mse)
-            mae = np.mean(np.abs(predictions - true_values))
-            epsilon = 1e-8  # 작은 값을 더해 나눗셈의 안정성 보장
-            mape = np.mean(np.abs((predictions - true_values) / (true_values + epsilon))) * 100
-            return mse, rmse, mae, mape
-
-        # 채널 1에 대한 결과
-        mse1, rmse1, mae1, mape1 = calculate_metrics(predictions_channel1, true_values_channel1)
-        print(f"Channel 1\nMSE: {mse1:.4f}\nRMSE: {rmse1:.4f}\nMAE: {mae1:.4f}\nMAPE: {mape1:.4f}%\n")
-
-        # 채널 2가 있는 경우에만 결과 출력
-        if true_values_channel2 and predictions_channel2:
-            mse2, rmse2, mae2, mape2 = calculate_metrics(predictions_channel2, true_values_channel2)
-            print(f"Channel 2\nMSE: {mse2:.4f}\nRMSE: {rmse2:.4f}\nMAE: {mae2:.4f}\nMAPE: {mape2:.4f}%")
+        print(f"Classification Metrics\nAccuracy: {accuracy:.4f}\nPrecision: {precision:.4f}\nRecall: {recall:.4f}\nF1 Score: {f1:.4f}\nAUROC: {auroc:.4f}")
+        print("\nConfusion Matrix:")
+        print(conf_matrix)
